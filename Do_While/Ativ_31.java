cb026703f9d78392989b8f4635fa233f8dd12764
Matheus Henrique C Barbosa <matheus.henrique.campos0408@gmail.com>
2025-02-07 17:14:36 -0300

    Add files via upload

---
+++ Do_While/Ativ_31.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ativ_31 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x;
+        do {
+            System.out.println("Digite um numero: ");
+            x = sc.nextInt();
+        }while (x>0);
+    }
+}
